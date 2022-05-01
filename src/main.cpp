5631df9962579fcc889cc7908ada30c6aac34353
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2022-04-30 22:09:26 -0500

    se modifica el código para agragar atributos a las zonas y un estado general del sistema

--- src/main.cpp
+++ src/main.cpp
@@ -14,41 +14,78 @@ int blue_light_pin = 18;
 enum SensorType
 {
   NORMALLY_OPEN,
-  NORMALLY_CLOSED,
-  NOT_USED
+  NORMALLY_CLOSED
 };
 
-enum ZoneStatus
+namespace Zone
 {
-  TROUBLE,
-  NORMAL,
-  ALARM,
-  UNKNOWN
-};
 
-ZoneStatus generalStatus = ZoneStatus::UNKNOWN;
+  enum Status
+  {
+    TROUBLE,
+    NORMAL,
+    ALARM,
+    UNDEFINED
+  };
 
-struct Zone
+  enum Definition
+  {
+    NO_USED,  // The zone will not operate in any way. Zones that are not used should be programmed as Null zones
+    DELAY,    // If this zone is violated when the panel is armed it will provide entry delay
+    INSTANT,  // If this zone type is violated when the panel is armed it will cause an instant alarm. Typically this zone is used for windows, patio doors or other perimeter type zones
+    INTERIOR, // If this type of zone is violated when the panel is armed it will provide entry if a delay type zone was violated first. Otherwise it will cause an instant alarm. Typically this zone is used for interior protection devices, such as motion detectors.
+    ARMED     // If this type of zone is violated when the panel is armed it will cause an instant alarm. Typically this zone is used for 24 hour zones.
+  };
+
+  enum Attributes
+  {
+    AUDIBLE,
+    SILENT,
+    PULSED
+  };
+
+  struct Config
+  {
+    int pin;
+    String zone_label;
+    long interval;
+    long last_time;
+    Status status;
+    SensorType sensor_type;
+    Definition definition;
+    bool chime;
+    Attributes attributes;
+  };
+
+}
+
+enum SystemArmedStatus
 {
-  int pin;
-  String zone_label;
-  long interval;
-  long last_time;
-  ZoneStatus status;
-  SensorType sensor_type;
+  READY, // The system is disarmed and ready to be armed
+  NOT_READY, // The system is disarmed, open zones are not ready to be armed
+  ARMED, // The system is armed and ready to go
+  ARMED_MEMORY_ALARM, // An alarm occurred while the system was armed.
+  ARMED_FORCED, // System armed but with some zone open or in trouble
+  UNDEFINED
 };
 
-Zone zones[2] = {
-    {Zone01, "Zone 01", 500, 0, ZoneStatus::UNKNOWN, SensorType::NORMALLY_CLOSED},
-    {Zone02, "Zone 02", 1500, 0, ZoneStatus::UNKNOWN, SensorType::NOT_USED}
-    //{Zone03, "Zone 03", 5000, 0, ZoneStatus::UNKNOWN},
+struct SystemStatus{
+  SystemArmedStatus armed_status;
+  Zone::Status zone_status;
+};
+
+SystemStatus system_status = {SystemArmedStatus::UNDEFINED, Zone::Status::UNDEFINED};
+
+Zone::Config zones[2] = {
+    {Zone01, "Zone 01", 500, 0, Zone::Status::UNDEFINED, SensorType::NORMALLY_CLOSED, Zone::Definition::INSTANT, true, Zone::Attributes::AUDIBLE},
+    {Zone02, "Zone 02", 1500, 0, Zone::Status::UNDEFINED, SensorType::NORMALLY_OPEN, Zone::Definition::NO_USED, false, Zone::Attributes::PULSED}
 };
 
 void GetZoneStatus(int numzone);
 
 void GetZoneStatus(int numzone)
 {
-  if (millis() - zones[numzone].last_time > zones[numzone].interval && zones[numzone].sensor_type != SensorType::NOT_USED)
+  if (millis() - zones[numzone].last_time > zones[numzone].interval && zones[numzone].definition != Zone::Definition::NO_USED)
   {
     zones[numzone].last_time = millis();
 
@@ -58,46 +95,23 @@ void GetZoneStatus(int numzone)
 
     if (valuez > 4000 && zones[numzone].sensor_type == SensorType::NORMALLY_CLOSED)
     {
-      zones[numzone].status = ZoneStatus::NORMAL;
+      zones[numzone].status = Zone::Status::NORMAL;
     }
     else if (valuez > 1700 && valuez < 1900 && zones[numzone].sensor_type == SensorType::NORMALLY_OPEN)
     {
-      zones[numzone].status = ZoneStatus::NORMAL;
+      zones[numzone].status = Zone::Status::NORMAL;
     }
     else if (valuez > 4000 && zones[numzone].sensor_type == SensorType::NORMALLY_OPEN)
     {
-      zones[numzone].status = ZoneStatus::ALARM;
+      zones[numzone].status = Zone::Status::ALARM;
     }
     else if (valuez > 1700 && valuez < 1900 && zones[numzone].sensor_type == SensorType::NORMALLY_CLOSED)
     {
-      zones[numzone].status = ZoneStatus::ALARM;
+      zones[numzone].status = Zone::Status::ALARM;
     }
     else
     {
-      zones[numzone].status = ZoneStatus::TROUBLE;
-    }
-
-    switch (zones[numzone].status)
-    {
-    case ZoneStatus::ALARM:
-      Serial.println("ALARM! " + zones[numzone].zone_label);
-      //   digitalWrite(LED, HIGH);
-      // delay(1000);
-      break;
-    case ZoneStatus::TROUBLE:
-      Serial.println("TROUBLE!! " + zones[numzone].zone_label);
-      //    digitalWrite(LED, HIGH);
-      // delay(500);
-      // digitalWrite(LED, LOW);
-      // delay(500);
-      break;
-    case ZoneStatus::NORMAL:
-      Serial.println("NORMAL " + zones[numzone].zone_label);
-      // digitalWrite(LED, HIGH);
-      // delay(1000);
-      // digitalWrite(LED, LOW);
-      // delay(1000);
-      break;
+      zones[numzone].status = Zone::Status::TROUBLE;
     }
   }
 }
@@ -149,57 +163,57 @@ void setup()
 
 void GeneralStatusLed()
 {
-  generalStatus = ZoneStatus::UNKNOWN;
-  int nz = sizeof(zones) / sizeof(Zone);
+  system_status.zone_status = Zone::Status::UNDEFINED;
+  int nz = sizeof(zones) / sizeof(Zone::Config);
 
   // Verifica si hay alguna zona con problema
   for (int i = 0; i < nz; i++)
   {
-    if (zones[i].status == ZoneStatus::TROUBLE)
+    if (zones[i].status == Zone::Status::TROUBLE)
     {
-      generalStatus = ZoneStatus::TROUBLE;
+      system_status.zone_status = Zone::Status::TROUBLE;
       break;
     }
   }
 
-  if (generalStatus == ZoneStatus::UNKNOWN)
+  if (system_status.zone_status == Zone::Status::UNDEFINED)
   {
     // Verifica si hay alguna zona en alarma
     for (int i = 0; i < nz; i++)
     {
-      if (zones[i].status == ZoneStatus::ALARM)
+      if (zones[i].status == Zone::Status::ALARM)
       {
-        generalStatus = ZoneStatus::ALARM;
+        system_status.zone_status = Zone::Status::ALARM;
         break;
       }
     }
   }
 
-  if (generalStatus == ZoneStatus::UNKNOWN)
+  if (system_status.zone_status == Zone::Status::UNDEFINED)
   {
     // Verifica si hay alguna zona en alarma
     for (int i = 0; i < nz; i++)
     {
-      if (zones[i].status == ZoneStatus::NORMAL)
+      if (zones[i].status == Zone::Status::NORMAL)
       {
-        generalStatus = ZoneStatus::NORMAL;
+        system_status.zone_status = Zone::Status::NORMAL;
         break;
       }
     }
   }
 
-  switch (generalStatus)
+  switch (system_status.zone_status)
   {
-  case ZoneStatus::ALARM:
+  case Zone::Status::ALARM:
     RGB_color(255, 0, 0); // Red
     break;
-  case ZoneStatus::TROUBLE:
+  case Zone::Status::TROUBLE:
     RGB_color(0, 0, 255); // Blue
     break;
-  case ZoneStatus::NORMAL:
+  case Zone::Status::NORMAL:
     RGB_color(0, 255, 0); // Green
     break;
-  case ZoneStatus::UNKNOWN:
+  case Zone::Status::UNDEFINED:
     RGB_color(0, 0, 0); // Blue
     break;
   }
@@ -207,7 +221,7 @@ void GeneralStatusLed()
 
 void loop()
 {
-  int nz = sizeof(zones) / sizeof(Zone);
+  int nz = sizeof(zones) / sizeof(Zone::Config);
 
   // Sondea el estado de cada zona
   for (int i = 0; i < nz; i++)
