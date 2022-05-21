fd239e7f33ad0d70b6fc6d3128375891126ce9e5
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2022-05-20 23:49:04 -0500

    Pruebas de conexión de SDCard

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,35 @@
 #include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h>
 #include "CtrlMaster.cpp"
+/*
+  SD card test for esp32
+ 
+  This example shows how use the utility libraries
+ 
+  The circuit:
+    SD card attached to SPI bus as follows:
+        SS    = 5;
+        MOSI  = 23;
+        MISO  = 19;
+        SCK   = 18;
+ 
+ 
+   by Mischianti Renzo <https://www.mischianti.org>
+  
+   https://www.mischianti.org
+*/
+// include the SD library:
+#include <SPI.h>
+#include <SD.h>
+// WeMos D1 esp8266: D8 as standard
+const int chipSelect = SS;
+ 
+void printDirectory(File dir, int numTabs);
+ 
 
 // Set Wifi AP - these to your desired credentials.
 const char *ssid = "edwinspire";
-const char *password = "1234567";
+const char *password = "Caracol1980";
 
 // WiFiServer server(80);
 
@@ -313,6 +338,86 @@ void setup()
   Serial.begin(115200);
   system_status.armed_status = SystemArmedStatus::UNDEFINED;
   WifiWebSocketServerSetup();
+  // BTESetup();
+
+  Serial.print("\nInitializing SD card...");
+ 
+  // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  if (!SD.begin(SS)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    while (1);
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+ 
+  // print the type of card
+  Serial.println();
+  Serial.print("Card type:         ");
+  switch (SD.cardType()) {
+    case CARD_NONE:
+      Serial.println("NONE");
+      break;
+    case CARD_MMC:
+      Serial.println("MMC");
+      break;
+    case CARD_SD:
+      Serial.println("SD");
+      break;
+    case CARD_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+ 
+  // print the type and size of the first FAT-type volume
+//  uint32_t volumesize;
+//  Serial.print("Volume type is:    FAT");
+//  Serial.println(SDFS.usefatType(), DEC);
+ 
+  Serial.print("Card size:  ");
+  Serial.println((float)SD.cardSize()/1000);
+ 
+  Serial.print("Total bytes: ");
+  Serial.println(SD.totalBytes());
+ 
+  Serial.print("Used bytes: ");
+  Serial.println(SD.usedBytes());
+ 
+  File dir =  SD.open("/");
+  printDirectory(dir, 0);
+
+}
+
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+ 
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.print(entry.size(), DEC);
+      time_t lw = entry.getLastWrite();
+      struct tm * tmstruct = localtime(&lw);
+      Serial.printf("\tLAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+    }
+    entry.close();
+  }
 }
 
 void loop()
@@ -321,11 +426,12 @@ void loop()
   EZ_OUT_01.loop();
   EZ_OUT_02.loop();
 
-    // Serial.println("Status " + String(system_status.armed_status));
+  // Serial.println("Status " + String(system_status.armed_status));
   // Serial.println("Status Last " + String(system_status_last.armed_status));
   if (SystemStatusLoop() || system_status.alarm_zone != system_status_last.alarm_zone || system_status.armed_status != system_status_last.armed_status || system_status.trouble_zone != system_status_last.trouble_zone)
   {
     notifyClients();
   }
   // RFIDloop();
+  // BTELoop();
 }
